06b5f69e38199b16439f0d66ff0a502edf57c5f7
IMD246 <tduy24699@gmail.com>
2021-06-13 01:08:34 +0700

    Up code Project c#

---
+++ QLTV(DOaN)/DAL/ConnectDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace DAL
+{
+    public class ConnectDB
+    {
+        string strCon = @"Data Source=DESKTOP-TBBANQL\SQLEXPRESS01;Initial Catalog = QuanLyThuVien; Integrated Security = True";
+        public SqlConnection sql = new SqlConnection("");
+
+        public ConnectDB()
+        {
+            sql.ConnectionString = strCon;
+            try
+            {
+                sql.Open();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        //public SqlConnection sql = new SqlConnection(@"Data Source=.;Initial Catalog=QuanLyThuVien;Integrated Security=True");
+
+        //public ConnectDB()
+        //{
+        //    try
+        //    {
+        //        sql.Open();
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        throw e;
+        //    }
+        //}
+    }
+}
